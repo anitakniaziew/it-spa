8e718069229242e5eaa6a0f9d6f1e0d1fadb478e
Anita Kowalska <anita.kniaziew@gmail.com>
2021-04-11 17:44:15 +0200

    Configure Eslint, Prettier

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,68 +1,65 @@
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const autoprefixer = require("autoprefixer");
-const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[contenthash].bundle.js'
+    filename: '[contenthash].bundle.js',
   },
-  devServer:{
+  devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 9000,
-    watchContentBase: true
+    watchContentBase: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      template: './src/index.html',
     }),
     new CleanWebpackPlugin(),
     new webpack.LoaderOptionsPlugin({
       options: {
-        postcss: [
-          autoprefixer()
-        ] 
-      }
-    })
+        postcss: [autoprefixer()],
+      },
+    }),
   ],
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        { loader: 'style-loader/url' },
-        { loader: "file-loader",
-        options: { name: "[name].css" }},
-        "sass-loader",
-        "postcss-loader"
-      ]},
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader/url' },
+          { loader: 'file-loader', options: { name: '[name].css' } },
+          'sass-loader',
+          'postcss-loader',
+        ],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            name: "[name].[ext]"
-          }
-        }
+            name: '[name].[ext]',
+          },
+        },
       },
       {
         test: /\.(html)$/,
-        use: ["html-loader"]
-      }
-    ]
-  }
-}
-
-
+        use: ['html-loader'],
+      },
+    ],
+  },
+};
