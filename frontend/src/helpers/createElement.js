0d415d92f3c329e137ccce1eaf832ee414fde064
Anita Kowalska <anita.kniaziew@gmail.com>
2021-04-17 23:36:42 +0200

    Implement createElement helper function

---
+++ frontend/src/helpers/createElement.js
@@ -0,0 +1,10 @@
+const createElement = (tagName, { classNames, children }) => {
+  const el = document.createElement(tagName);
+
+  classNames?.forEach((className) => el.classList.add(className));
+  children?.forEach((child) => el.append(child));
+
+  return el;
+};
+
+export default createElement;
