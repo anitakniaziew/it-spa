70e8be619ac85ca9965365004bed3084d4407e92
Anita Kowalska <anita.kniaziew@gmail.com>
2021-05-23 11:35:13 +0200

    Fix loader positioning in reservations

--- frontend/src/components/loader.js
+++ frontend/src/components/loader.js
@@ -4,6 +4,7 @@ const div = (classNames) => createElement('div', { classNames });
 const span = (classNames) => createElement('span', { classNames });
 
 const loader = () => createElement('div', {
+  classNames: ['d-flex', 'justify-content-center'],
   children: [
     div(['spinner-grow', 'text-info', 'mr-3'], span(['sr-only'])),
     div(['spinner-grow', 'text-info', 'mr-3'], span(['sr-only'])),
